f43a11be57a9b97643629150d9fdd962b47a38a6
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-13 17:40:35 -0400

    POM for account page

--- DemoProjectForSelenium/AccountPage.cs
+++ DemoProjectForSelenium/AccountPage.cs
@@ -18,6 +18,21 @@ namespace DemoProjectForSelenium
         //identify the search text box element by the id value as search_query_top
         [FindsBy(How = How.Id, Using = "search_query_top")]
         public IWebElement txtSearch { get; set; }
+        //identify the search button with name value as submit_search
+        [FindsBy(How = How.Name, Using = "submit_search")]
+        public IWebElement btnSearch { get; set; }
+        //shopping cart
+        [FindsBy(How = How.ClassName, Using = "shopping_cart")]
+        public IWebElement shpCart { get; set; }
+
+        //women tab
+        [FindsBy(How = How.ClassName, Using = "sf-with-ul")]
+        public IWebElement vmenTab { get; set; }
+        //dresses
+        [FindsBy(How = How.ClassName, Using = "sf-with-ul")]
+        public IWebElement dressTab { get; set; }
+
+
 
 
     }
